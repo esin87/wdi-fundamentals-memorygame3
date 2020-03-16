640a3cec5936791f3271a57afa266167ed31d99e
wrightw2055 <56096914+wrightw2055@users.noreply.github.com>
2020-03-16 17:00:16 -0400

    create game logic

--- memory-game/JS/main.js
+++ memory-game/JS/main.js
@@ -1,7 +1,50 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards =["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne[cardId]);
 console.log("User flipped " + cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo[cardId]);
 console.log("User flipped " + cardTwo);
+function printFlipcard (cardId, cardOne, cardTwo,)
+console.log("User flipped " + cards[cardId])
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+const myArray = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+}
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+}
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png",
+}
+
+];
+function createBoard ()
+for (let i = 0; i < createBoard.length; i++) {
+    // Logic here
+    let cardOne = document.createElement("images/queen-of-hearts.png");
+    let cardTwo = document.createElement("images/queen-of-diamonds.png");
+    cardElement.setAttribute("cards","images/back.png");
+    cardElement.setAttribute('data-id',"images/back.png");
+  addEventListener()  
+
+}
